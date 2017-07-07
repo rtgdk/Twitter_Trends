05547eb638a9e5afdc9da904dd4f881cd717b4f8
rtgdk <rohit.lodhartg@gmail.com>
2017-07-07 13:12:30 +0530

    60

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	url(r'^ajaxtweets/(?P<woeid>[-\w]+)/(?P<hashtag>[-\w]+)/(?P<count>[-\w]+)/$',views.tweet_fetch, name ="Fetch tweets"),
 	url(r'^search/$', views.autocompleteModel , name = 'search'),
 	url(r'^hashtag/(?P<hasht>[-\w]+)/$', views.hashtag , name = 'hashtag'),
+	url(r'^moretweets/(?P<hashtag>[-\w]+)/(?P<currt>[-\w]+)$', views.moretweets , name = 'moretweets'),
 	#url(r'^other/$', views.other, name="other"),
 ]
